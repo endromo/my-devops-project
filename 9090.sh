6a00c67d53862393ba2ae81df290760d89a8a5d6
endro <endro.mono9@gmail.com>
2026-06-22 16:54:47 +0700

    Update version

---
+++ 9090.sh
@@ -0,0 +1,2 @@
+export POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/name=prometheus,app.kubernetes.io/instance=prometheus" -o jsonpath="{.items[0].metadata.name}")
+kubectl --namespace monitoring port-forward $POD_NAME 9090:9090
